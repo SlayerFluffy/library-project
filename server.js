1bd55b8a63f7f36a546a6a25d401d279e77cbb15
SlayerFluffy <wjezek@byupathway.edu>
2026-04-02 17:47:21 -0700

    attempting render.com fix

--- server.js
+++ server.js
@@ -1,47 +1,50 @@
-require ('dotenv').config();
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+if (process.env.NODE_ENV !== "production") {
+  try {
+    require("dotenv").config();
+  } catch {
+    console.warn("dotenv not found; using existing environment variables.");
+  }
+}
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var requestLogger = require('./middleware/requestLogger');
-var notFound = require('./middleware/notFound');
-var errorHandler = require('./middleware/errorHandler');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var requestLogger = require("./middleware/requestLogger");
+var notFound = require("./middleware/notFound");
+var errorHandler = require("./middleware/errorHandler");
 
 var app = express();
 var port = process.env.PORT || 3000;
 
-const mongodb = require('./data/database');
+const mongodb = require("./data/database");
 
+app.set("views", path.join(__dirname, "views"));
 
-app.set('views', path.join(__dirname, 'views'));
-
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(requestLogger);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 app.use(notFound);
 app.use(errorHandler);
 
-
 //MongoDB connection
-mongodb.initDb((err, mongodb) => {
-    if (err) {
-        console.log(err);
-    } else 
-    {
-        app.listen(port, () => {
-            console.log(`Server is running on port ${port}`);
-        });
-    }
+mongodb.initDb((err, _mongodb) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  }
 });
 
 module.exports = app;
