d61cb51bba3902f631e6f64e5cb639c181ae344c
Ojeaga <ojeagabclifford@gmail.com>
2026-04-06 10:15:35 +0100

    work on the Oauth of the api

--- routes/books.js
+++ routes/books.js
@@ -2,10 +2,14 @@ const express = require("express");
 const router = express.Router();
 const booksController = require("../controllers/booksController");
 
+const { isAuthenticated } = require("../middleware/authenticate");
+
+// isAuthenticated (ADD IN POST, PUT, DELETE)
+
 router.get("/", booksController.getAllBooks);
 router.get("/:id", booksController.getBookById);
-router.post("/", booksController.createBook);
-router.put("/:id", booksController.updateBook);
-router.delete("/:id", booksController.deleteBook);
+router.post("/", isAuthenticated, booksController.createBook);
+router.put("/:id", isAuthenticated,booksController.updateBook);
+router.delete("/:id",isAuthenticated, booksController.deleteBook);
 
 module.exports = router;
