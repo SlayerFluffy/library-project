2165f7bdb9fa5c08ae4e7e0697f5d01bc2dc2b6e
Ojeagabclifford <168076351+Ojeagabclifford@users.noreply.github.com>
2026-04-10 13:37:02 +0100

    Add passport middleware to index routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
 var indexController = require("../controllers/indexController");
+const passport = require('passport');
 
 router.use("/", require("./swagger"));
 
