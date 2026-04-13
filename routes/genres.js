0debf47b86354a568434a17ee2cbb83098d3b0c6
Ojeagabclifford <168076351+Ojeagabclifford@users.noreply.github.com>
2026-04-13 09:54:25 +0100

    Add authentication middleware to genre routes

--- routes/genres.js
+++ routes/genres.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/genresController');
 const validation = require('../middleware/validation');
+const { isAuthenticated } = require("../middleware/authenticate");
 
 router.get('/', controller.getAllGenres);
 router.get('/:id', validation.validateObjectId, controller.getGenreById);
-router.post('/', validation.saveGenre, controller.createGenre);
-router.put('/:id', validation.validateObjectId, validation.saveGenre, controller.updateGenre);
-router.delete('/:id', validation.validateObjectId, controller.deleteGenre);
+router.post('/',isAuthenticated, validation.saveGenre, controller.createGenre);
+router.put('/:id', isAuthenticated, validation.validateObjectId, validation.saveGenre, controller.updateGenre);
+router.delete('/:id', isAuthenticated,validation.validateObjectId, controller.deleteGenre);
 
 module.exports = router;
